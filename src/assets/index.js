873e6ef3722e15c058bc4b728d5985c1cc39e3ac
Shubham Jayswal <shubhammasai03@gmail.com>
2024-03-17 09:29:14 +0530

    Navabr created

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import Rectangle18 from "./Rectangle18.png"
+import LogoSaviour from "./LogoSaviour.png"
+import menu from "./menu.png"
+import dropdown from "./dropdown.png"
+import intrologo from "./intrologo.png"
+import roadmap from "./roadmap.png"
+import card1 from "./card1.png"
+import card2 from "./card2.png"
+import card3 from "./card3.png"
+import card4 from "./card4.png"
+import tokenomicsbg from "./tokenomics-bg.png"
+import tokenomics from "./tokenomics.png"
+export { Rectangle18, LogoSaviour, menu, dropdown, intrologo, roadmap, card1, card2, card3, card4, tokenomicsbg, tokenomics} 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { LogoSaviour, dropdown, menu } from "../assets";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+function Navbar() {
+    return (
+        <div className="fixed flex justify-between items-center h-[117px] w-full p-[27px] p-[5%] top-[27px] bg-transparent z-50">
+            {/* Left side - Logo and Saviour text */}
+            <div className="flex items-center gap-4">
+                {/* Show menu icon for small screens */}
+                <img
+                    src={menu}
+                    className="w-10 h-10 cursor-pointer block md:hidden"
+                    alt="menu"
+                />
+                <img
+                    src={LogoSaviour}
+                    className="object-cover w-[100px] h-[111px] mt-8"
+                    alt="Logo Saviour"
+                />
+                <p className="hidden md:block text-white font-zcool text-[1.2rem]">Saviour</p>
+            </div>
+
+            {/* Center - Navigation items */}
+            <div className="hidden md:flex max-w-[583px] h-[52px] bg-[#181918] flex items-center gap-[24px] justify-center p-[16px] p-[24px] text-white rounded-[20px]">
+                {/* Show navigation items for small screens */}
+                <span className="font-zcool text-[1rem] hover:text-red-500 cursor-pointer block bg-transparent">Home</span>
+                <span className="font-zcool text-[1rem] hover:text-red-500 cursor-pointer block bg-transparent">IDO</span>
+                <span className="font-zcool text-[1rem] hover:text-red-500 cursor-pointer block bg-transparent">Tokenomics</span>
+                <span className="font-zcool text-[1rem] hover:text-red-500 cursor-pointer block bg-transparent">Roadmap</span>
+                <span className="flex items-center gap-1 font-zcool text-[1rem] hover:text-red-500 cursor-pointer block bg-transparent">Coming Soon</span>
+                <span>
+                    <img src={dropdown} alt="dropdown" className="w-[16px] h-[16px] " />
+                </span>
+            </div>
+
+            {/* Right side - Connect Wallet button */}
+            <button className="bg-gradient-to-r from-[#ED0137] to-[#EE6910] rounded-[1.1rem] p-[10px] text-[12px] text-white shadow-lg h-[50px] w-[137px]">
+                Connect Wallet
+            </button>
+        </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
